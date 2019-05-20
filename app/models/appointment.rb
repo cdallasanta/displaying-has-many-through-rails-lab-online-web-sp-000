ac94d696e9f3050b5e2566c6431a84c228f6c8fd
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-05-20 21:30:23 +0000

    Automatically backed up by Learn

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,6 +3,6 @@ class Appointment < ApplicationRecord
   belongs_to :patient
 
   def readable_format
-    self.appointment_datetime.strftime("%B %-d, %y at )
+    self.appointment_datetime.strftime("%B %-d, %y at %H:%M")
   end
 end
